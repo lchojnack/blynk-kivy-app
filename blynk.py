595b6ebb7767edaf1050c00b385b3a0cee98df4c
Lukasz Chojnacki <lukasz.chojnacki@pwr.edu.pl>
2017-12-22 23:58:19 +0100

    Android buildozer.spec and info if the lamp is on

--- blynk.py
+++ blynk.py
@@ -8,14 +8,15 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
 
-from blynkapi import Blynk
+from kivy.network.urlrequest import UrlRequest
+from urllib2 import Request, urlopen
 
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '100')
 
 class BlynkApp(App):
-    _auth_token = "your_token_here"
-    _desk_lamp = Blynk(_auth_token, pin="d17")
+    _desk_lamp_token = "your_token"
+    _desk_lamp_pin = "V1"
 
     def build(self):
         self.title="Smart room application"
@@ -28,18 +29,57 @@ class BlynkApp(App):
         desk_lamp_btn_off = Button(text='Turn Off')
         desk_lamp_btn_off.bind(on_press=self.desk_lamp_off)
 
-        layout.add_widget(Label(text="Desk Lamp"))
+        value = self.blynk_get(self._desk_lamp_token, self._desk_lamp_pin)
+        self.desk_lamp_label = Label(text="Desk Lamp\nis {}".format(value),
+                                     halign="center"
+                                     )
+        layout.add_widget(self.desk_lamp_label)
         layout.add_widget(desk_lamp_btn_on)
         layout.add_widget(desk_lamp_btn_off)
 
+
+        # night_lamp_btn_on =  Button(text='Turn On')
+        # night_lamp_btn_on.bind(on_press=self.night_lamp_on)
+        #
+        # night_lamp_btn_off = Button(text='Turn Off')
+        # night_lamp_btn_off.bind(on_press=self.night_lamp_off)
+        #
         # layout.add_widget(Label(text="Night Lamp"))
+        # layout.add_widget(night_lamp_btn_on)
+        # layout.add_widget(night_lamp_btn_off)
+
         return layout
 
+    def blynk_update(self, token, pin, value):
+        url = r"http://blynk-cloud.com/{}/update/{}?value={}".format(token, pin, value)
+        request = UrlRequest(url)
+        request.wait()
+        # request = Request(url)
+
+    def blynk_get(self, token, pin):
+        url = r"http://blynk-cloud.com/{}/get/{}".format(token, pin)
+        request = Request(url)
+        response_body = urlopen(request).read()
+        if "1" in response_body:
+            return "On"
+        else:
+            return "Off"
+
     def desk_lamp_on(self, event):
-        self._desk_lamp.on()
+        self.blynk_update(self._desk_lamp_token, self._desk_lamp_pin, 1)
+        value = self.blynk_get(self._desk_lamp_token, self._desk_lamp_pin)
+        self.desk_lamp_label.text = "Desk Lamp\nis {}".format(value)
 
     def desk_lamp_off(self, event):
-        self._desk_lamp.off()
+        self.blynk_update(self._desk_lamp_token, self._desk_lamp_pin, 0)
+        value = self.blynk_get(self._desk_lamp_token, self._desk_lamp_pin)
+        self.desk_lamp_label.text = "Desk Lamp\nis {}".format(value)
+
+    # def night_lamp_on(self, event):
+    #     pass
+    #
+    # def night_lamp_off(self, event):
+    #     pass
 
 if __name__ == '__main__':
     BlynkApp().run()
